5305b7c3f25c30671ed5de015973f65c955f3170
MangaD <MangaD@users.noreply.github.com>
2025-07-21 20:44:16 +0000

    deploy: aa95651737bfc3c6e8159365d93f72e0065addc8

--- search/functions_4.js
+++ search/functions_4.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['enablebroadcast_0',['enableBroadcast',['../classjsocketpp_1_1DatagramSocket.html#ab3ad0faf65e430cc8867d76c6a84df87',1,'jsocketpp::DatagramSocket']]],
-  ['enablekeepalive_1',['enableKeepAlive',['../classjsocketpp_1_1Socket.html#a4c37f604fb8534760e00e077f5c4c5cf',1,'jsocketpp::Socket']]],
-  ['enablenodelay_2',['enableNoDelay',['../classjsocketpp_1_1Socket.html#ac2975ed8e1d02b5f823bc0af50c034c7',1,'jsocketpp::Socket']]]
+  ['enablekeepalive_1',['enableKeepAlive',['../classjsocketpp_1_1Socket.html#aaceee2a0a6204cbc3e1481303891fcab',1,'jsocketpp::Socket']]],
+  ['enablenodelay_2',['enableNoDelay',['../classjsocketpp_1_1Socket.html#a4be91b4d9fd6da1ce2b36de1158e707c',1,'jsocketpp::Socket']]]
 ];
